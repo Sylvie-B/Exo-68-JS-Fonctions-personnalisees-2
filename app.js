2ebf6d2cadca24d78fb8f462aa4f5da315bc6ada
Sylvie-B <unsolo@hotmail.fr>
2020-12-02 15:57:21 +0100

    sans console

--- app.js
+++ app.js
@@ -1,12 +1,9 @@
 let elementP = document.getElementsByTagName("p");
-console.log(elementP);
 
 function alea() {
     return Math.ceil(Math.random()*100);
 }
-console.log(alea());
 
 for(let i = 0; i < elementP.length; i++){
     elementP.item(i).innerHTML = alea().toString();
 }
-
